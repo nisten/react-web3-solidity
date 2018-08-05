58ed2c71e83af7c72ae5ce551ba117100313b8a4
nisten <nisten@outlook.com>
2018-08-05 18:32:50 -0400

    Web3 version shows on homescreen

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,10 +6,14 @@ import Web3 from 'web3'
 const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
 
 console.log(Web3.version);
-web3.eth.getBlock(3150).then(console.log);
+//web3.eth.getBlock(3150).then(console.log);
 
 export default withSiteData(() => (
   <div>
+    <div className = "ethBalance">
+      Curerntly running web3.js version 
+      <button>{web3.version}</button>
+    </div>
     <h1 style={{ textAlign: 'center' }}>react-redux-solidity</h1>
     <img src={logoImg} alt="" />
   </div>
